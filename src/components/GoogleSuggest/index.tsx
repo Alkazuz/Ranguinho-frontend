8fa53e2887c683fa2970b76386b37c415481813e
vcfernandes <vinnerr02@hotmail.com>
2022-11-01 20:45:08 -0400

    .

--- src/components/GoogleSuggest/index.tsx
+++ src/components/GoogleSuggest/index.tsx
@@ -2,8 +2,9 @@ import React, {Component} from "react"
 import ReactGoogleMapLoader from "react-google-maps-loader"
 import ReactGooglePlacesSuggest from "react-google-places-suggest"
 
-const MY_API_KEY = "AIzaSyB7YL4z-L-sgk9-qp9iYxiv7br-ITaU9zI" // fake
+import './index.css'
 
+const MY_API_KEY = "AIzaSyB7YL4z-L-sgk9-qp9iYxiv7br-ITaU9zI" // fake
 export default class GoogleSuggest extends React.Component {
     state = {
         search: "",
@@ -50,14 +51,20 @@ export default class GoogleSuggest extends React.Component {
                             // Optional props
                             onNoResult={this.handleNoResult}
                             onSelectSuggest={this.handleSelectSuggest}
-                            onStatusUpdate={this.handleStatusUpdate}
+                            onStatusUpdate={this.handleStatusUpdate}    
                             textNoResults="My custom no results text" // null or "" if you want to disable the no results item
                             customRender={prediction => (
-                                <div className="customWrapper">
+                                <div className="locationContainer">
+                                    <div className="locationIcon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                        <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 256c-35.3 0-64-28.7-64-64s28.7-64 64-64s64 28.7 64 64s-28.7 64-64 64z"/></svg>
+                                    <div className="locationInfo"></div>
+                                    
                                     {prediction
                                         ? prediction.description
                                         : "My custom no results text"}
                                 </div>
+                                </div>
+                                
                             )}
                         >
                             <input
