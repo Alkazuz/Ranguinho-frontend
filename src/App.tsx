4182bc4a6a3c61b5a22770de1855e18fcc9698fb
Marcos <kerallis2015@gmail.com>
2022-11-03 09:25:32 -0300

    feat: :sparkles: Novas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Welcome from './pages/welcome'
 import Restaurante from './pages/restaurante'
+import PhoneLogin from './pages/login/PhoneLogin'
+import LoginMain from './pages/login/LoginMain'
+import EmailLogin from './pages/login/EmailLogin'
+import Restaurantes from './pages/restaurantes'
 
 function App() {
 
@@ -11,7 +15,15 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Welcome />} />
-        <Route path='/restaurante' element={<Restaurante />} />
+        <Route path='/inicio' element={<Welcome />} />
+
+        <Route path='/restaurante/:id' element={<Restaurante />} />
+        <Route path='/restaurantes' element={<Restaurantes />} />
+
+        <Route path='/login' element={<LoginMain />} />
+        <Route path='/login/celular' element={<PhoneLogin />} />
+        <Route path='/login/email' element={<EmailLogin />} />
+
       </Routes>
     </Router>
   )
