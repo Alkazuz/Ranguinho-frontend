2aa4070c847c4e3180773365cabf0b97fc70bac0
Marcos <kerallis2015@gmail.com>
2022-11-08 10:56:03 -0300

    feat: :zap: Melhoria na reponsividade

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000/',
+    baseURL: 'http://127.0.0.1:3000/v1/',
     // baseURL: "https://jsonplaceholder.typicode.com",
 });
 
